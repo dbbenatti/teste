689cd1e229a6144263c959f317969d949de96a6a
Wesley Forti <wesley.forti@digiage.com.br>
2018-05-18 02:55:08 +0000

    TASK4.java edited online with Bitbucket

--- myinterview/src/main/java/com/example/TASK4.java
+++ myinterview/src/main/java/com/example/TASK4.java
@@ -2,7 +2,7 @@ package com.example;
 
 /**
  * Create an implementation of a Rest API client.
- * Prints out how many records exits for each gender and save this file to s3 bucket
+ * Prints out how many records exists for each gender and save this file to s3 bucket
  *
  */
 public class TASK4 {
